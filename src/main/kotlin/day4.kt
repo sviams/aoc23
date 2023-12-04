4914a54b21e8aac77ce7bbea3abc88ac04037d50
Anders Jändel <sviams@gmail.com>
2023-12-04 15:40:00 +0100

    Day 4 simplify

--- src/main/kotlin/day4.kt
+++ src/main/kotlin/day4.kt
@@ -1,39 +1,25 @@
-import kotlin.math.max
-import kotlin.math.min
-
 object day4 {
 
     data class Card(val id: Int, val winning: List<Int>, val mine: List<Int>) {
         val matches by lazy { mine.intersect(winning) }
         val value by lazy {
-            if (matches.isNotEmpty())
+            if (matches.isEmpty()) 0 else
                 ( 0 until matches.size-1).fold(1) { acc, i -> acc * 2 }
-            else 0
         }
-        val copies by lazy { matches.indices.fold(emptyList<Int>()) { acc, i ->
-            acc.plus(id+i+1)
-            }
-        }
+        val copies by lazy { matches.indices.fold(emptyList<Int>()) { acc, i -> acc.plus(id+i+1) } }
     }
 
     fun parseInput(input: List<String>): List<Card> = input.fold(emptyList<Card>()) { acc, line ->
         val (id) = Regex("""Card\s+(\d+):""").find(line)!!.destructured
         val parts = line.split(':').last().split('|')
-        val winningA = parts[0].split(' ').filterNot { it.isEmpty() }
-        val winning = winningA.map { it.toInt() }
-        val mineA = parts[1].split(' ').filterNot { it.isEmpty() }
-        val mine = mineA.map { it.toInt() }
+        val winning = parts[0].split(' ').filterNot { it.isEmpty() }.map { it.toInt() }
+        val mine = parts[1].split(' ').filterNot { it.isEmpty() }.map { it.toInt() }
         acc + Card(id.toInt(), winning, mine)
-    }
-
-    tailrec fun calcValues(unprocessed: List<Card>, result: Map<Int, Int> = emptyMap()): Int {
-        if (unprocessed.isEmpty()) return result.values.sum()
-        val next = unprocessed.first()
-        val v = 1 + next.copies.sumOf { result[it]!! }
-        return calcValues(unprocessed.drop(1), result + (next.id to v))
     }
 
     fun pt1(input: List<String>): Int = parseInput(input).sumOf { it.value }
 
-    fun pt2(input: List<String>): Int = calcValues(parseInput(input).reversed())
+    fun pt2(input: List<String>): Int = parseInput(input)
+        .reversed()
+        .fold(emptyMap<Int, Int>()) { res, card -> res + (card.id to (1 + card.copies.sumOf { res[it]!! }))}.values.sum()
 }
